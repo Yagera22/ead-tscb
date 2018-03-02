9ccda8186acc07c8faf7438567db78c19364a985
Yagera22 <36978120+Yagera22@users.noreply.github.com>
2018-03-02 12:26:30 -0500

    Add files via upload

---
+++ CustomerDetailsTDK/Form1.Designer.cs
@@ -0,0 +1,375 @@
+﻿namespace CustomerDetailsTDK
+{
+    partial class formMovie
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formMovie));
+            this.txtFName = new System.Windows.Forms.TextBox();
+            this.txtLName = new System.Windows.Forms.TextBox();
+            this.lblFName = new System.Windows.Forms.Label();
+            this.lblLName = new System.Windows.Forms.Label();
+            this.lblGender = new System.Windows.Forms.Label();
+            this.rdbMale = new System.Windows.Forms.RadioButton();
+            this.rdbFemale = new System.Windows.Forms.RadioButton();
+            this.lblStreet = new System.Windows.Forms.Label();
+            this.txtStreet = new System.Windows.Forms.TextBox();
+            this.lblCity = new System.Windows.Forms.Label();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.lblParish = new System.Windows.Forms.Label();
+            this.lblTotVid = new System.Windows.Forms.Label();
+            this.txtTotVid = new System.Windows.Forms.TextBox();
+            this.cboxParish = new System.Windows.Forms.ComboBox();
+            this.cboxPackage = new System.Windows.Forms.ComboBox();
+            this.lblSubPackage = new System.Windows.Forms.Label();
+            this.rdbNo = new System.Windows.Forms.RadioButton();
+            this.rdbYes = new System.Windows.Forms.RadioButton();
+            this.lblVideo = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pBoxMovie = new System.Windows.Forms.PictureBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxMovie)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtFName
+            // 
+            this.txtFName.Location = new System.Drawing.Point(84, 73);
+            this.txtFName.Name = "txtFName";
+            this.txtFName.Size = new System.Drawing.Size(205, 20);
+            this.txtFName.TabIndex = 0;
+            // 
+            // txtLName
+            // 
+            this.txtLName.Location = new System.Drawing.Point(84, 110);
+            this.txtLName.Name = "txtLName";
+            this.txtLName.Size = new System.Drawing.Size(205, 20);
+            this.txtLName.TabIndex = 1;
+            // 
+            // lblFName
+            // 
+            this.lblFName.AutoSize = true;
+            this.lblFName.Location = new System.Drawing.Point(14, 76);
+            this.lblFName.Name = "lblFName";
+            this.lblFName.Size = new System.Drawing.Size(57, 13);
+            this.lblFName.TabIndex = 2;
+            this.lblFName.Text = "First Name";
+            // 
+            // lblLName
+            // 
+            this.lblLName.AutoSize = true;
+            this.lblLName.Location = new System.Drawing.Point(14, 113);
+            this.lblLName.Name = "lblLName";
+            this.lblLName.Size = new System.Drawing.Size(58, 13);
+            this.lblLName.TabIndex = 3;
+            this.lblLName.Text = "Last Name";
+            // 
+            // lblGender
+            // 
+            this.lblGender.AutoSize = true;
+            this.lblGender.Location = new System.Drawing.Point(14, 148);
+            this.lblGender.Name = "lblGender";
+            this.lblGender.Size = new System.Drawing.Size(42, 13);
+            this.lblGender.TabIndex = 4;
+            this.lblGender.Text = "Gender";
+            // 
+            // rdbMale
+            // 
+            this.rdbMale.AutoSize = true;
+            this.rdbMale.Location = new System.Drawing.Point(113, 148);
+            this.rdbMale.Name = "rdbMale";
+            this.rdbMale.Size = new System.Drawing.Size(48, 17);
+            this.rdbMale.TabIndex = 5;
+            this.rdbMale.Text = "Male";
+            this.rdbMale.UseVisualStyleBackColor = true;
+            // 
+            // rdbFemale
+            // 
+            this.rdbFemale.AutoSize = true;
+            this.rdbFemale.Location = new System.Drawing.Point(204, 148);
+            this.rdbFemale.Name = "rdbFemale";
+            this.rdbFemale.Size = new System.Drawing.Size(59, 17);
+            this.rdbFemale.TabIndex = 6;
+            this.rdbFemale.Text = "Female";
+            this.rdbFemale.UseVisualStyleBackColor = true;
+            // 
+            // lblStreet
+            // 
+            this.lblStreet.AutoSize = true;
+            this.lblStreet.Location = new System.Drawing.Point(14, 185);
+            this.lblStreet.Name = "lblStreet";
+            this.lblStreet.Size = new System.Drawing.Size(35, 13);
+            this.lblStreet.TabIndex = 8;
+            this.lblStreet.Text = "Street";
+            // 
+            // txtStreet
+            // 
+            this.txtStreet.Location = new System.Drawing.Point(84, 182);
+            this.txtStreet.Name = "txtStreet";
+            this.txtStreet.Size = new System.Drawing.Size(205, 20);
+            this.txtStreet.TabIndex = 7;
+            // 
+            // lblCity
+            // 
+            this.lblCity.AutoSize = true;
+            this.lblCity.Location = new System.Drawing.Point(14, 220);
+            this.lblCity.Name = "lblCity";
+            this.lblCity.Size = new System.Drawing.Size(24, 13);
+            this.lblCity.TabIndex = 10;
+            this.lblCity.Text = "City";
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(84, 217);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(205, 20);
+            this.txtCity.TabIndex = 9;
+            // 
+            // lblParish
+            // 
+            this.lblParish.AutoSize = true;
+            this.lblParish.Location = new System.Drawing.Point(14, 255);
+            this.lblParish.Name = "lblParish";
+            this.lblParish.Size = new System.Drawing.Size(36, 13);
+            this.lblParish.TabIndex = 11;
+            this.lblParish.Text = "Parish";
+            // 
+            // lblTotVid
+            // 
+            this.lblTotVid.AutoSize = true;
+            this.lblTotVid.Location = new System.Drawing.Point(342, 150);
+            this.lblTotVid.Name = "lblTotVid";
+            this.lblTotVid.Size = new System.Drawing.Size(99, 13);
+            this.lblTotVid.TabIndex = 13;
+            this.lblTotVid.Text = "Total Video Rented";
+            // 
+            // txtTotVid
+            // 
+            this.txtTotVid.Location = new System.Drawing.Point(457, 148);
+            this.txtTotVid.Name = "txtTotVid";
+            this.txtTotVid.Size = new System.Drawing.Size(128, 20);
+            this.txtTotVid.TabIndex = 12;
+            // 
+            // cboxParish
+            // 
+            this.cboxParish.FormattingEnabled = true;
+            this.cboxParish.Items.AddRange(new object[] {
+            "Kingston",
+            "Portland",
+            "St. Mary",
+            "St. Ann",
+            "St. Andrew",
+            "Trelawny",
+            "St. James",
+            "Hanover",
+            "St. Elizabeth",
+            "St. Thomas",
+            "Clarendon",
+            "St. Catherine",
+            "Manchester",
+            "Westmoreland"});
+            this.cboxParish.Location = new System.Drawing.Point(84, 252);
+            this.cboxParish.Name = "cboxParish";
+            this.cboxParish.Size = new System.Drawing.Size(121, 21);
+            this.cboxParish.TabIndex = 14;
+            this.cboxParish.Text = "Select Parish";
+            // 
+            // cboxPackage
+            // 
+            this.cboxPackage.FormattingEnabled = true;
+            this.cboxPackage.Items.AddRange(new object[] {
+            "Access Package",
+            "DISH Movie Package",
+            "TV Plus",
+            "TV Essential"});
+            this.cboxPackage.Location = new System.Drawing.Point(457, 76);
+            this.cboxPackage.Name = "cboxPackage";
+            this.cboxPackage.Size = new System.Drawing.Size(128, 21);
+            this.cboxPackage.TabIndex = 16;
+            this.cboxPackage.Text = "Select Package";
+            // 
+            // lblSubPackage
+            // 
+            this.lblSubPackage.AutoSize = true;
+            this.lblSubPackage.Location = new System.Drawing.Point(335, 79);
+            this.lblSubPackage.Name = "lblSubPackage";
+            this.lblSubPackage.Size = new System.Drawing.Size(106, 13);
+            this.lblSubPackage.TabIndex = 15;
+            this.lblSubPackage.Text = "Subscribed Package";
+            // 
+            // rdbNo
+            // 
+            this.rdbNo.AutoSize = true;
+            this.rdbNo.Location = new System.Drawing.Point(525, 111);
+            this.rdbNo.Name = "rdbNo";
+            this.rdbNo.Size = new System.Drawing.Size(39, 17);
+            this.rdbNo.TabIndex = 19;
+            this.rdbNo.Text = "No";
+            this.rdbNo.UseVisualStyleBackColor = true;
+            // 
+            // rdbYes
+            // 
+            this.rdbYes.AutoSize = true;
+            this.rdbYes.Location = new System.Drawing.Point(457, 111);
+            this.rdbYes.Name = "rdbYes";
+            this.rdbYes.Size = new System.Drawing.Size(43, 17);
+            this.rdbYes.TabIndex = 18;
+            this.rdbYes.Text = "Yes";
+            this.rdbYes.UseVisualStyleBackColor = true;
+            // 
+            // lblVideo
+            // 
+            this.lblVideo.AutoSize = true;
+            this.lblVideo.Location = new System.Drawing.Point(349, 113);
+            this.lblVideo.Name = "lblVideo";
+            this.lblVideo.Size = new System.Drawing.Size(92, 13);
+            this.lblVideo.TabIndex = 17;
+            this.lblVideo.Text = "Video on Demand";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Red;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(165, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(303, 41);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Customer Details";
+            // 
+            // pBoxMovie
+            // 
+            this.pBoxMovie.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pBoxMovie.Image = ((System.Drawing.Image)(resources.GetObject("pBoxMovie.Image")));
+            this.pBoxMovie.Location = new System.Drawing.Point(352, 180);
+            this.pBoxMovie.Name = "pBoxMovie";
+            this.pBoxMovie.Size = new System.Drawing.Size(233, 120);
+            this.pBoxMovie.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBoxMovie.TabIndex = 21;
+            this.pBoxMovie.TabStop = false;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(354, 306);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 22;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(434, 306);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 23;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(512, 306);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 24;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // formMovie
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(597, 341);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.pBoxMovie);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rdbNo);
+            this.Controls.Add(this.rdbYes);
+            this.Controls.Add(this.lblVideo);
+            this.Controls.Add(this.cboxPackage);
+            this.Controls.Add(this.lblSubPackage);
+            this.Controls.Add(this.cboxParish);
+            this.Controls.Add(this.lblTotVid);
+            this.Controls.Add(this.txtTotVid);
+            this.Controls.Add(this.lblParish);
+            this.Controls.Add(this.lblCity);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.lblStreet);
+            this.Controls.Add(this.txtStreet);
+            this.Controls.Add(this.rdbFemale);
+            this.Controls.Add(this.rdbMale);
+            this.Controls.Add(this.lblGender);
+            this.Controls.Add(this.lblLName);
+            this.Controls.Add(this.lblFName);
+            this.Controls.Add(this.txtLName);
+            this.Controls.Add(this.txtFName);
+            this.Name = "formMovie";
+            this.Text = "Movie Night Form";
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxMovie)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFName;
+        private System.Windows.Forms.TextBox txtLName;
+        private System.Windows.Forms.Label lblFName;
+        private System.Windows.Forms.Label lblLName;
+        private System.Windows.Forms.Label lblGender;
+        private System.Windows.Forms.RadioButton rdbMale;
+        private System.Windows.Forms.RadioButton rdbFemale;
+        private System.Windows.Forms.Label lblStreet;
+        private System.Windows.Forms.TextBox txtStreet;
+        private System.Windows.Forms.Label lblCity;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.Label lblParish;
+        private System.Windows.Forms.Label lblTotVid;
+        private System.Windows.Forms.TextBox txtTotVid;
+        private System.Windows.Forms.ComboBox cboxParish;
+        private System.Windows.Forms.ComboBox cboxPackage;
+        private System.Windows.Forms.Label lblSubPackage;
+        private System.Windows.Forms.RadioButton rdbNo;
+        private System.Windows.Forms.RadioButton rdbYes;
+        private System.Windows.Forms.Label lblVideo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pBoxMovie;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSubmit;
+    }
+}
+
---
+++ CustomerDetailsTDK/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Tariq Kellyman #1600197760
+//Sachell Anderson #
+//Cleon Whittick #
+//Brandon Chan #
+
+namespace CustomerDetailsTDK
+{
+    public partial class formMovie : Form
+    {
+        public formMovie()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtFName.Text = String.Empty;
+            txtLName.Text = String.Empty;
+            txtStreet.Text = String.Empty;
+            txtCity.Text = String.Empty;
+            txtTotVid.Text = String.Empty;
+            rdbFemale.Checked = false;
+            rdbMale.Checked = false;
+            rdbNo.Checked = false;
+            rdbYes.Checked = false;
+            cboxPackage.Text = String.Empty;
+            cboxParish.Text = String.Empty;
+
+           
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            String name = "Name:\t" + txtFName.Text + " " + txtLName.Text;
+            String address = "Address:\t" + txtStreet.Text + ", " + txtCity.Text + ", " + cboxParish.Text;
+            String package = "Package & Videos:\t" + cboxPackage.Text + ", " +  txtTotVid.Text;
+
+            String vod = "";
+            bool isCheckedVOD = rdbYes.Checked;
+            if (isCheckedVOD)
+                vod = rdbYes.Text;
+            else
+                vod = rdbNo.Text;
+
+            String gender = "";
+            bool isCheckedGen = rdbMale.Checked;
+            if (isCheckedGen)
+                gender = rdbMale.Text;
+            else
+                gender = rdbFemale.Text;
+
+            MessageBox.Show(name + "\n" + address + "\n" + "Gender:\t" + gender + "\n" + package + "\n" + "VOD:\t" + vod);
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+    }
+}
